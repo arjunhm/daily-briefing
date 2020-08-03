862a5cb48bb9a488b0532c52051a81dba36e1c01
Arjun H M <hm.arjun97@gmail.com>
2020-08-03 12:16:07 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,21 @@
+
+# Reddit API
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+CLIENT_AGENT = ""
+POST_LIMIT = 10
+SUBREDDITS = [""]
+
+# Chrome Settings
+CHROME_PATH = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+WEBPAGE_URLS = ["https://techcrunch.com/", "https://news.ycombinator.com/", "https://calendar.google.com/calendar/r", "https://ticktick.com/webapp/#q/all/tasks"]
+
+# Football API
+FOOTBALL_API_TOKEN = ""
+
+# Weather API
+WEATHER_LOCATION = "Bangalore"
+WEATHER_API_KEY = ""
+
+# Stock Market
+STOCKS = ["RELIANCE", "ASIANPAINT", "BHEL", "FEDERALBNK"]
