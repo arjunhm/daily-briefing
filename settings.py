56862078f0c99e9ba5e3bff72e6ddb59e5db2c9b
Arjun H M <hm.arjun97@gmail.com>
2020-08-03 12:16:29 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ SUBREDDITS = [""]
 
 # Chrome Settings
 CHROME_PATH = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-WEBPAGE_URLS = ["https://techcrunch.com/", "https://news.ycombinator.com/", "https://calendar.google.com/calendar/r", "https://ticktick.com/webapp/#q/all/tasks"]
+WEBPAGE_URLS = ["https://techcrunch.com/", "https://news.ycombinator.com/"]
 
 # Football API
 FOOTBALL_API_TOKEN = ""
